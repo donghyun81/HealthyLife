9ac81ece0fc02245a7d040d4fa310818692daf72
donghyun <donghyunyoon81@gmail.com>
2021-12-22 20:37:55 +0900

    12/22commit

--- app/src/main/java/com/example/healthylife/data/calendar_exercise/CalendarExercise.kt
+++ app/src/main/java/com/example/healthylife/data/calendar_exercise/CalendarExercise.kt
@@ -1,12 +1,9 @@
 package com.example.healthylife.data.calendar_exercise
 
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.example.healthylife.data.exercise.Exercise
 import java.util.*
 
-@Entity
-data class CalendarExercise (
-    val selectedDay:String,
-    val exercise:Exercise,
-    val timer:Timer,
-        )
